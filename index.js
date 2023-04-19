cf0a429d60745cd33f888de797a16cd0b47a32b7
Sebastian Sala <sebastian_orko@hotmail.com>
2023-04-18 23:15:39 -0600

    project done, removeAt added

--- index.js
+++ index.js
@@ -8,51 +8,141 @@ class LinkedList{
 
     append(value){
         const node = new Node(value);
-        this.tailNode.nextNode = node;
-        this.tailNode = node;
-        this.size++;
+        if (this.tailNode === null) {
+            this.headNode = node;
+            this.tailNode = node;
+        } else {
+            this.tailNode.nextNode = node;
+            this.tailNode = node;
+        }
+            this.size++;
     }
 
     prepend(value){
         const node = new Node(value);
-        this.headNode.nextNode = node;
-        this.nextNode = node;
+        if(this.headNode === null){
+            this.headNode = node;
+            this.tailNode = node;
+        }else{
+            node.nextNode = this.headNode;
+            this.headNode = node;
+        }
         this.size++;
     }
 
-    size(){
+    getSize(){
         return this.size;
     }
 
-    head(){
+    getHead(){
         return this.headNode
     }
 
-    tail(){
+    getTail(){
         return this.tailNode
     }
-
-    at(index){
-
+    
+    getAt(index){
+        if(this.headNode === null){
+            return;
+        }
+        let counter = 0;
+        let node = this.headNode;
+        while(node !== null){
+            if(counter === index){
+                return node;
+            }else{
+                node = node.nextNode
+                counter++;
+            }
+        }
     }
-
-    pop(){
-        const node = new Node()
-
+    
+    popNode(){
+        if(this.headNode === null){
+            return;
+        }
+        let previousNode = null;
+        let currentNode = this.headNode;
+        while(currentNode.nextNode !== null){
+            previousNode = currentNode;
+            currentNode = currentNode.nextNode;
+        }
+        if(currentNode === null){
+            return
+        }
+        previousNode.nextNode = currentNode.nextNode;
+        this.tailNode = previousNode;
         this.size--;
-
     }
-
+    
     contains(value){
-
+        if(this.headNode === null){
+            return;
+        }
+        let node = this.headNode;
+        while(node !== null){
+            if(node.value === value){
+                return true
+            }else{
+                node = node.nextNode
+            }
+        }
+        return false;
     }
-
+    
     find(value){
-
+        if(this.headNode === null){
+            return;
+        }
+        let node = this.headNode;
+        let counter = 0;
+        while(node !== null){
+            if(node.value === value){
+                return counter;
+            }else{
+                node = node.nextNode;
+                counter++;
+            }
+        }
+        return null;
+    }
+    
+    toString(){
+        if(this.headNode === null){
+            return;
+        }
+        let node = this.headNode;
+        let string = '';
+        while(node !== null){
+            string += `( ${node.value} ) ->`;
+            node = node.nextNode
+        }
+        return string;
     }
 
-    toString(){
-
+    removeAt(index){
+        if(this.headNode === null){
+            return;
+        }
+        let prev = null;
+        let current = this.headNode;
+        let counter = 0;
+        while(current !== null){
+            if(counter === index){
+                if(current === this.headNode){
+                    this.headNode = current.nextNode;
+                }
+                this.tailNode = prev;
+                prev.nextNode = null;
+                this.size--;
+                break;
+            }else{
+                prev = current;
+                current = current.nextNode;
+                counter++;
+            }
+        }
     }
 }
 
